be6c23e88865d67ebe264ad7de308b1d3a70b113
JonShort <JonShort@users.noreply.github.com>
2020-05-31 10:05:46 +0100

    tiny refactor - removed unused field

--- exercises/tax-calculator/tax.go
+++ exercises/tax-calculator/tax.go
@@ -18,11 +18,10 @@ func init() {
 }
 
 type band struct {
-	amount        int
-	maxTax        int
-	rate          int
-	taxableAmount int
-	whenMax       int
+	amount  int
+	maxTax  int
+	rate    int
+	whenMax int
 }
 
 func main() {
@@ -30,9 +29,9 @@ func main() {
 
 	var bands [4]band
 
-	bands[0] = band{amount: 12500, rate: 0, whenMax: 12500, taxableAmount: 0}
-	bands[1] = band{amount: 50000, rate: 20, whenMax: 30000, taxableAmount: 37500, maxTax: 7500}
-	bands[2] = band{amount: 150000, rate: 40, whenMax: 60000, taxableAmount: 100000, maxTax: 40000}
+	bands[0] = band{amount: 12500, rate: 0, whenMax: 12500, maxTax: 0}
+	bands[1] = band{amount: 50000, rate: 20, whenMax: 30000, maxTax: 7500}
+	bands[2] = band{amount: 150000, rate: 40, whenMax: 60000, maxTax: 40000}
 	bands[3] = band{amount: 0x7FF0000000000000, rate: 45}
 
 	postTax := 0
@@ -44,23 +43,27 @@ func main() {
 		if i == 0 {
 			if income <= band.amount {
 				postTax += income
+
 				break
 			} else {
 				postTax += band.whenMax
+
 				continue
 			}
 		}
 
-		prevBand := bands[i-1]
-
 		if income > band.amount {
 			taxPaid += band.maxTax
 			postTax += band.whenMax
+
 			continue
 		} else {
+			prevBand := bands[i-1]
+
 			tax := ((income - prevBand.amount) * band.rate) / 100
 			taxPaid += tax
 			postTax += (income - prevBand.amount) - tax
+
 			break
 		}
 	}
